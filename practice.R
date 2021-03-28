39a2974a6b7aa84a0a9c85cea97a0b272ac793f4
jinheo89 <80936457+jinheo89@users.noreply.github.com>
2021-03-28 15:35:39 +0900

    Add files via upload

--- practice.R
+++ practice.R
@@ -9,10 +9,14 @@ gen2 <- filter(gendata,gendata[,2]==2, .preserve = FALSE )
 #1호기 2호기의 시간대별 발전 추출하기
 
 time<-gen1[,1]
+y<-as.Date(time) #as.date 함수로 문자열을 날짜로 변환
 generation<-gen1[,"X8"]
-dt<-data.frame(time,generation)
+dt<-data.frame(y,generation)
+str(dt)
 library(xts)
-power<-xts(dt$generation, order.by = dt$time)
+power<-xts(dt$generation, order.by = dt$y)
+dygraph(power)
+
 
 
 
