f62c01dfe069ca297e8840e9b098b83d4bb2f6b5
jinheo89 <80936457+jinheo89@users.noreply.github.com>
2021-03-28 19:21:29 +0900

    Add files via upload

--- practice.R
+++ practice.R
@@ -1,29 +1,34 @@
 setwd("C:/Rworks")
 gendata<-read.csv("solor_power_generation.csv", header = TRUE)
 gendata
+#문제정의 : 낮의 길이가 가장 긴 여름에 가장 발전량이 가장 많지 않을까? 
 
 
+#1호기 2호기의 시간대별 발전량 추출하기
 library(dplyr)
 gen1 <- filter(gendata,gendata[,2]==1, .preserve = FALSE )
 gen2 <- filter(gendata,gendata[,2]==2, .preserve = FALSE )
-#1호기 2호기의 시간대별 발전 추출하기
 
 time<-gen1[,1]
-x<-as.Date(time) #as.date 함수로 문자열을 날짜로 변환
+#as.date 함수로 문자열을 날짜로 변환
+x<-as.Date(time) 
 generation<-gen1[,"X8"]
 dt<-data.frame(x,generation)
 str(dt)
+
+#시계열 데이터로 전환
 library(xts)
 power<-xts(dt$generation, order.by = dt$x)
+
+#데이터 시각화
+library(dygraphs)
 dygraph(power)
 
+#처음 가설을 세워 데이터를 분석하고 시각화하여 결론을 도출했다.
+#봄과 가을이 발전량이 가장 많고 여름이 그다음 겨울이 가장 적다.
+#태양광 모듈 온도가 상승하면 효율이 떨어진다. 
 
 
 
-library(ggplot2)
-library(xts)
-library(dygraphs)
-str(economics)
-eco<-xts(economics$unemploy, order.by = economics$date)
-dygraph(eco)
+
 
